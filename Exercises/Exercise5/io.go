bf6aa1a060ab649a21c0a9fc1d1c3dcb69465e6e
aarskog <eparis@atlassian.com>
2016-02-11 13:26:32 +0100

    test

--- Exercises/Exercise5/io.go
+++ Exercises/Exercise5/io.go
@@ -1,17 +1,31 @@
 
 package elevatorDriver
 
-func io_init(){}
+import "C"
 
-func io_set_bit() {
+// New text here
+
+
+func io_init() bool{ 
+	C.io_init()
 }
 
-func io_clear_bit() {
-	
+func io_set_bit(channel int) {
+	C.io_set_bit(C.int(channel)
 }
 
-func io_read_bit(){}
+func io_clear_bit(channel int) {
+	C.io_clear_bit(C.int(channel)
+}
 
-func io_read_analog(){}
+func io_read_bit(channel int) int{
+	return int( C.io_read_bit(C.int(channel)))
+}
 
-func io_write_analog(){}
+func io_read_analog(channel int) int{
+	return int(C.io_read_analog(C.int(channel)))
+}
+
+func io_write_analog(channel, value int){
+	C.io_write_analog(C.int(channel), C.int(value))
+}
