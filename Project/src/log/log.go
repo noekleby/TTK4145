0974a57ac00f4beb475dca809e2f9bb56fe97689
Magnus Pedersen <mnp_94_@hotmail.com>
2016-03-04 13:27:18 +0100

    Updated log.go temporarily

--- Project/src/log/log.go
+++ Project/src/log/log.go
@@ -1,3 +1,29 @@
 //print error etc. 
 
 package log
+
+import (
+    "fmt"
+    "log"
+    "strings"
+)
+
+type Level int
+const (
+    Debug Level = iota
+    Info
+    Error
+    Fatal
+)
+
+const log_level Level = Info
+const module_filter = ""
+
+func Println(level Level, module string, args...interface{}) {
+    if level >= log_level && (module_filter == "" || strings.Contains(module_filter, module)) {
+        fmt.Printf("%s", fmt.Sprintf("%s\t+\t%s", module, fmt.Sprintln(args...)))
+    }
+    if level == Fatal {
+        log.Fatal("Program terminated")
+    }
+}
