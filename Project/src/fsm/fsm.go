d0b1aeee875b74762637b1dac5c90786d5604a0e
Ida Reistad Abrahamsson <Ida@dhcp-10-22-65-101.wlan.ntnu.no>
2016-04-07 13:43:10 +0200

    Added states in FSM

--- Project/src/fsm/fsm.go
+++ Project/src/fsm/fsm.go
@@ -1,2 +1,50 @@
-// Finite state machine
 package fsm
+
+import(
+	"fmt"
+	"../driver"
+	"time"
+)
+
+const(
+	IDLE = iota
+	ELEVATING 
+	DOOR_OPEN 
+)
+
+type State int
+
+type ElevatorState struct{
+	fsmState State
+	floor, dir, destination int 
+}
+
+func (elev *ElevatorState) IDLE() { // we have to use pointer reciever beacause we want to read and write as oposed to just read. 	
+	fmt.Println("Going to state: IDLE")
+	driver.StopElevate()
+	elev.fsmState = IDLE
+}
+
+func (elev *ElevatorState) Elevating(direction int) {
+	fmt.Println("Getting in to motion by going to state: ELEVATING")
+	if direction == 1 {
+		driver.ElevateUp()
+	} else {
+		driver.ElevateDown()
+	}
+	elev.fsmState = ELEVATING
+
+}
+
+func (elev *ElevatorState) DoorOpen() {
+	fmt.Printline "Going in to state: DOOR_OPEN"
+	driver.StopElevate()
+	elev.fsmState = DOOR_OPEN
+	driver.elevSetDoorOpenLamp(1)
+	time.sleep(400*time.Milliseconds) //The lines under should happend after sleep. 
+	driver.elevSetDoorOpenLamp(0)
+	elev.IDLE()
+}
+
+
+
