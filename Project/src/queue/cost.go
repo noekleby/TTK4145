8cc9579afeb1f79271df32e8152fe59114472def
sanntid <student@stud.ntnu.no>
2016-04-07 16:46:45 +0200

    a

--- Project/src/queue/cost.go
+++ Project/src/queue/cost.go
@@ -1,3 +1,33 @@
 // Calculating cost for all elevators
 package queue
 
+import(
+	"../driver"
+)
+
+func Cost (currFloor, targetFloor, direction int) int{
+	cost := 0
+	if currFloor == -1 { //Hvis heisen er mellom etasjer
+		cost++
+	}
+	else if direction != 0 { //Heis på etasje, men i full fart
+		cost += 2
+	}
+	if currFloor < targetFloor {
+		for floor := currFloor; floor <= N_FLOORS; floor++ {
+			cost++
+		}
+		if direction < 0 {
+			cost += 5
+		}
+	}
+	if currFloor > targetFloor {
+		for floor := N_FLOORS; floor => currFloor; floor-- {
+			cost++
+		}
+		if direction > 0 {
+			cost += 5 
+		}
+	}
+	return cost
+}
