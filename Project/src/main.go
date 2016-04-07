2dc31869316e1844c46c5eabc95d6b940d48e22e
sanntid <student@stud.ntnu.no>
2016-04-07 16:46:27 +0200

    a

--- Project/src/main.go
+++ Project/src/main.go
@@ -3,17 +3,14 @@ package main
 import (
 	"./driver"
 	"fmt"
-	//"time"
 )
 
 func main() {
-	if driver.elev_init() == 0 {
+	if driver.Init() == 0 {
 		fmt.Println("The elevator was not able to initialize")
 	}
-	if driver.elev_init() == 1 {
+	if driver.Init() == 1 {
 		fmt.Println("The elevator was able to initialize")
 	}
 }
 
-// cannot refer to unexported name driver.elev_init
-// ./main.go:10: undefined: driver.elev_init  :(
