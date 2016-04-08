7ac8755999c03c33fb59071f5400a9a9b43a1127
Magnus Pedersen <mnp_94_@hotmail.com>
2016-04-08 14:01:46 +0200

    a

--- Project/src/queue/queue.go
+++ Project/src/queue/queue.go
@@ -4,52 +4,62 @@ import (
 	"../eventHandler"
 	"../driver"
 )
-//var cost_matrix = [driver.N_FLOORS*driver.N_BUTTONS]
 
 func EmptyQueue bool {
 	for f := 0; f < driver.N_FLOORS; f++ {
 		for b := 0; b < driver.N_BUTTONS; b++ {
-			if (driver.Msg.ExternalUp[i] == 0 || driver.Msg.ExDownOrders[i] == 0 || driver.Msg.InOrders[i] == 0) {
+			if Msg.ExternalUp[i] == 0 || Msg.ExDownOrders[i] == 0 || Msg.InOrders[i] == 0 {
 				return true
 			}
 		}
 	}
 	return false
 }
-
-/*func (q* queue) sortQueue int(){
-	for i := 0; i < length(q); i++ {
-		cost_matrix(i) = cost(currFloor, targetFloor, direction)
-	}
-	for j := 0; j < 
-}
-
-func NewDirection  int () {
-	if EmptyQueue {
-		driver.Init()
-	} else {
-
-	}
-
-}
-*/
-
 // 1:  first checks if it should stop in the current floor
 // 2:  if not, check orders furter in the current direction 
 // 3:  if not, check orders in opposite direction in current floor
 // 4:  if not, change direction 
 
-func changeDirection () int{
-	//Should stop (= 0) if emptyqueue is true
-	//Should stop if the new order is the same as the previous (you're in the same floor)
-	//Make if statement for msg = direction up
-	//	Make loop
-	//Make if statement for msg = direction down (or just use else)
-	//	Make loop
+func Direction () int{
+	if EmptyQueue == 0 {    //Should stop (= 0) if emptyqueue is true
+		return 0
+	}
+	if InternalOrders[PrevFloor] { 	//Should stop if the new order is the same as the previous (you're in the same floor)
+		return 0
+	}
+	if dir { // direction == up
+		//Check if (Prev.floor+1) which is current floor until the top. If we detect an order in the queue, the lift continues upward
+		for i := PrevFloor + 1; i < N_FLOORS; i++ {
+			if InternalOrders[i] || ExternalUp[i] || ExternalDown[i] {
+				return 1 //lift goes up
+			}
+		}
+		return -1
+	}
+	if dir == -1 { // or just use else
+		for i := PrevFloor - 1; i >= 0; i-- { // just the opposite of the previous if statement
+			if InternalOrders[i] || ExternalUp[i] || ExternalDown[i] {
+				return -1 //lift goes down
+			}
+		}
+		return 1
+	}
 }
 
-func ordersInDirection () bool {
-	//Checks first in up direction
-	//then in down direction using if statements
+func RemoveOrders() { //Orders that are finished
+	InternalOrders[PrevFloor] = 0
+	SetButtonLamp(PrevFloor,0,false) // resets previous floor which always will be done when function is called 
+	if dir { // Resets all orders in up direction
+		ExternalUp[PrevFloor] = 0
+		SetButtonLamp(PrevFloor,1,false)
+	}
+	if dir == -1 {
+		ExternalDown[PrevFloor] = 0
+		SetButtonLamp(PrevFloor,-1,false)
+	}
+}
+
+func AdddOrder() {
+	
 }
 
